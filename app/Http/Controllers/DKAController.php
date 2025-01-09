87fde92947848f309936c3b51886830268324d35
AlfiaFauziah <alfiafauziah8@gmail.com>
2025-01-09 12:23:26 +0700

    baruuu

---
+++ app/Http/Controllers/DKAController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DKAController extends Controller
+{
+    public function show($page)
+    {
+        // Check if the view exists to prevent errors
+        $validPages = ['absensi', 'beranda', 'index','resume', 'rekap', 'laporan_kecelakaan'];  // Add your valid pages here
+
+        if (in_array($page, $validPages)) {
+            return view('dkas.' . $page);
+        } else {
+            abort(404); // Page not found
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\UkkController;
 use App\Http\Controllers\K3Controller;
+use App\Http\Controllers\GAController;
+use App\Http\Controllers\DKAController;
 use App\Http\Controllers\LoginController;
 
 // // Dashboard Route with parameter 'page'
@@ -15,6 +17,12 @@ Route::get('ukks/{page}', [UkkController::class, 'show'])->name('ukks.show');
 
 // Dashboard Route with parameter 'page'
 Route::get('k3s/{page}', [K3Controller::class, 'show'])->name('k3s.show');
+
+// Dashboard Route with parameter 'page'
+Route::get('gas/{page}', [K3Controller::class, 'show'])->name('gas.show');
+
+// Dashboard Route with parameter 'page'
+Route::get('dkas/{page}', [K3Controller::class, 'show'])->name('dkas.show');
 
 // Login Routes
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
